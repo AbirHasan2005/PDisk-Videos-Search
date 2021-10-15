191e9a2d3b9e7054aab8dd4f2c36b3984432be68
Mufaz-TG <92558762+Mufaz-TG@users.noreply.github.com>
2021-10-15 17:25:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ async def text_handler(_, m: Message):
         traceback.print_exc()
         try: await editable.edit("Failed to search!",
                                  reply_markup=InlineKeyboardMarkup([
-                                     [InlineKeyboardButton("Sumpot Group", url="https://t.me/JoinOT")]
+                                     [InlineKeyboardButton("Support Group", url="https://t.me/JoinOT")]
                                  ]))
         except MessageNotModified: pass
     elif not response["data"]["list"]:
