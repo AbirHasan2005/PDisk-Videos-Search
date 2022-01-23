4e1f38c9729653c15769ef46f5f9e71ea18ce5d7
Kevin <76389377+kevinnadar22@users.noreply.github.com>
2022-01-23 17:25:20 +0530

    Update login.py

--- core/login.py
+++ core/login.py
@@ -8,13 +8,14 @@ from selenium.common.exceptions import (
     StaleElementReferenceException,
     ElementClickInterceptedException
 )
+from selenium.webdriver.common.by import By
 
 
 async def pdisk_login(username: str, password: str):
     print("Bot on Standby ...")
     login_url = "https://www.pdisk.net/login?type=login"
     get_id_page_url = "https://www.pdisk.net/withdraw"
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--no-sandbox")
@@ -24,11 +25,11 @@ async def pdisk_login(username: str, password: str):
     print("Trying to Login to PDisk Account ...")
     driver.get(login_url)
     print("Adding Username in Box ...")
-    driver.find_element_by_xpath("//input[@placeholder='Username/Email']").send_keys(username)
+    driver.find_element(By.XPATH, "//input[@placeholder='Username/Email']").send_keys(username)
     print("Adding Password in Box ...")
-    driver.find_element_by_xpath("//input[@placeholder='Password']").send_keys(password)
+    driver.find_element(By.XPATH, "//input[@placeholder='Password']").send_keys(password)
     print("Clicking Login Button ...")
-    driver.find_element_by_xpath("//button[@type='button']").click()
+    driver.find_element(By.XPATH, "//button[@type='button']").click()
     count = 0
     login_success = None
     while count < 4:
@@ -50,7 +51,7 @@ async def pdisk_login(username: str, password: str):
               f"Password: '{password}'")
         driver.get(get_id_page_url)
         try:
-            user_id = driver.find_element_by_class_name("account-id").text.split(' ', 1)[-1]
+            user_id = driver.find_element(By.CLASS_NAME , "account-id").text.split(' ', 1)[-1]
         except NoSuchElementException:
             user_id = None
         print(f"PDisk User ID: '{user_id}'")
